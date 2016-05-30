b0db0ba44ebfd6efe446c0c3ca83fc8ad9a9bc33
Matt Brodrecht <mbrodrecht@igloosoftware.com>
2016-05-29 20:42:25 -0400

    sets title variable to null if propery is undefined - won't render empty h2

--- app/js/components/Card.js
+++ app/js/components/Card.js
@@ -10,9 +10,11 @@ class Card extends React.Component {
     }
 
     render() {
+        const title = (this.props.title) ? <h2>{this.props.title}</h2> : null;
+        
         return (
             <div className="card">
-                <h2>{this.props.title}</h2>
+                {title}
                 <div className="card-content">
                     {this.props.children}
                 </div>
